7cc6c34801bb01463caef6104a8687904ff02194
Avery Bouchard <averybou@gmail.com>
2021-06-11 08:37:05 -0400

    added averages and totals

--- Week4/expandedAssignment4.c
+++ Week4/expandedAssignment4.c
@@ -21,12 +21,23 @@ int main()
     /* initialize variables */
     const int standardHours = 40; /* standard 40 hour work week */
     const float otRate = 1.5; /* multiply OT hours by this number */
+    const int employeesToProcess = 5; /* how many employees will need to be processed? */
     int idx;                /* loop index */
+
+    /* initialize variables to calculate totals and averages */
+    float wageTotal;
+    float wageAverage;
+    float hoursTotal; 
+    float hoursAverage;
+    float otTotal;
+    float otAverage;
+    float grossTotal;
+    float grossAverage;
+    
     /* initialize arrays */
     float gross[SIZE] = {};            /* gross pay for week (wage * hours) */
     float hoursWorked[SIZE] = {};    /* number of hours worked per week */
     float overtimeHours[SIZE] = {}; 
-    const int employeesToProcess = 5; /* how many employees will need to be processed? */
     long int clockNumber [SIZE] = {98401, 526488, 765349, 34645, 127615}; /* employee clock number */
     float hourlyWage [SIZE] = {10.6, 9.75, 10.5, 12.25, 8.35}; /* hourly wage */
 
@@ -53,8 +64,20 @@ int main()
         /* calculate gross pay */
         gross[idx] = hourlyWage[idx] * (hoursWorked[idx] - overtimeHours[idx]) + overtimeHours[idx] * otRate;
 
+        /* calculate totals */
+        wageTotal += hourlyWage[idx]; /* add employee wage to all previous employees wages */
+        hoursTotal += hoursWorked[idx]; /* add hours worked to total previous employees hours */
+        otTotal += overtimeHours[idx]; /* add overtime hours to all previous employees */
+        grossTotal += gross[idx]; /* add gross pay to all previous employees */
 
     }
+
+    /* calculate averages */
+    wageAverage = wageTotal / SIZE;
+    hoursAverage = hoursTotal / SIZE;
+    otAverage = otTotal / SIZE;
+    grossAverage = grossTotal / SIZE;
+
 
     /* start of table */
     printf("\n\tClock # | Wage | Hours | OT Hours | Gross\n");
@@ -66,6 +89,14 @@ int main()
         printf("\t%06li %8.2f %6.1f %7.2f %12.2f\n", clockNumber[idx], hourlyWage[idx], hoursWorked[idx], overtimeHours[idx], gross[idx]);
 
     }
+
+    printf("\t_______________________________________________________\n");
+
+    /* print totals */
+    printf("\n\tTotal %9.2f %6.1f %7.2f %12.2f\n", wageTotal, hoursTotal, otTotal, grossTotal);
+
+    /* print averages */
+    printf("\tAverage %7.2f %6.1f %7.2f %12.2f\n", wageAverage, hoursAverage, otAverage, grossAverage);
 
     printf("\n\n");
 
